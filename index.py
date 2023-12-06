c9147129ddd275a98666e3b313b621e033d55945
iaquino <iagodeaquino10@gmail.com>
2023-12-06 08:46:33 -0300

    sistema de cores adicionado

--- index.py
+++ index.py
@@ -23,19 +23,19 @@ def form_image(width,height,image,image_compare):
             new_image.putpixel((i,j), (image[i][j].r,image[i][j].g,image[i][j].b))
     new_image.save("painted.bmp")
 
-def img_to_binary_mat(image, width, height):
+def img_to_binary_mat(image, width, height, pos):
     mat_final = []
     for i in range(width):
         line = []
         for j in range(height):
-            if (image[i][j].r+image[i][j].g+image[i][j].b) >= 1:
+            if image[i][j].r == image[pos[0]][pos[1]].r and image[i][j].g == image[pos[0]][pos[1]].g and image[i][j].b == image[pos[0]][pos[1]].b:
                 line.append(1)
             else:
                 line.append(0)
         mat_final.append(line)
     return mat_final
 
-def mat_to_binary_img(mat,width, height):
+def mat_to_binary_img(mat,width, height, color):
     image_final = []
     for i in range(width):
         line_pixel = []
@@ -43,7 +43,7 @@ def mat_to_binary_img(mat,width, height):
             if mat[i][j] == 1:
                 line_pixel.append(Pixel(255,255,255))
             if mat[i][j] == 0:
-                line_pixel.append(Pixel(0,0,0))
+                line_pixel.append(Pixel(color[0],color[1],color[2]))
         image_final.append(line_pixel)
     return image_final
                 
@@ -65,48 +65,39 @@ def generate_structural_element(size,size_width, size_height):
     return mask
 
 def floodfill_Morfologico(image, pos_click, width, height, mask):
-        final_image = []
+    final_image = []
+    for i in range(width):
+        line = []
+        for j in range(height):
+            line.append(1)
+        final_image.append(line)
+    final_image[pos_click[0]][pos_click[1]] = 0
+    floodfill = Transformacoes(3,final_image,width, height)
+    floodfill.mask.mask = mask
+    while True:
+        image_comp = []
         for i in range(width):
             line = []
             for j in range(height):
-                line.append(1)
-            final_image.append(line)
-        final_image[pos_click[0]][pos_click[1]] = 0
-        floodfill = Transformacoes(3,final_image,width, height)
-        floodfill.mask.mask = mask
-        while True:
-            image_comp = []
-            for i in range(width):
-                line = []
-                for j in range(height):
-                    if final_image[i][j] == 0:
-                        line.append(0)
-                    else:
-                        line.append(1)
-                image_comp.append(line)
-            final_image = floodfill.apply_dilation()
-            final_image = floodfill.remove_intersection(width, height, image, final_image)
-            cont_diff = 0
-            different = False
-            for i in range(width):
-                for j in range(height):
-                    if final_image[i][j] != image_comp[i][j]:
-                        different = True
-                        cont_diff += 1
-            if different == False:
-                print("terminei")
-                break
-            floodfill.image = final_image
-        fused_images = []
-        for i in range(width):
-            line = []
-            for j in range(height):
-                if final_image[i][j] == 0 or image[i][j] == 0:
+                if final_image[i][j] == 0:
                     line.append(0)
                 else:
                     line.append(1)
-            fused_images.append(line)
-        return fused_images
+            image_comp.append(line)
+        final_image = floodfill.apply_dilation()
+        final_image = floodfill.remove_intersection(width, height, image, final_image)
+        cont_diff = 0
+        different = False
+        for i in range(width):
+            for j in range(height):
+                if final_image[i][j] != image_comp[i][j]:
+                    different = True
+                    cont_diff += 1
+        if different == False:
+            print("terminei")
+            break
+        floodfill.image = final_image
+    return final_image
 
 
             
@@ -131,19 +122,35 @@ def main():
     altura = 300
     tela = pygame.display.set_mode((largura,altura))
     tela.fill("white")
+    color_queue = []
+    color_queue.append((255,0,0))
+    color_queue.append((0,255,0))
+    color_queue.append((0,0,255))
+    color_queue.append((0,0,0))
+    color_pos = 0
     stop = False
+    circ = 0
     while run:
         for event in pygame.event.get():
             if event.type == QUIT:
                 run = False
             if event.type == MOUSEBUTTONDOWN:
                 pressed = True
+                atribute = event.__getattribute__("button")
+                if atribute == 5:
+                    color_pos+=1
+                    if color_pos > 3:
+                        color_pos = 0
+                elif atribute == 4:
+                    color_pos-=1
+                    if color_pos < 0:
+                        color_pos = 3
             if event.type == MOUSEBUTTONUP:
                 pressed = False
-
         if pressed == True:
             if pygame.mouse.get_pressed()[0] == True:
-                pygame.draw.rect(tela,"black", (pygame.mouse.get_pos()[0]-2,pygame.mouse.get_pos()[1]-2,4,4), width=0)
+                pygame.draw.rect(tela,color_queue[color_pos], (pygame.mouse.get_pos()[0]-2,pygame.mouse.get_pos()[1]-2,4,4), width=0)
+            
             if pygame.mouse.get_pressed()[2] == True and stop == False:
                 stop = True
                 imagem = []
@@ -153,13 +160,22 @@ def main():
                         pixel = tela.get_at((i,j))
                         line_pixel.append(Pixel(pixel[0],pixel[1], pixel[2]))
                     imagem.append(line_pixel)
-                mat = img_to_binary_mat(imagem, largura, altura)
+                mat = img_to_binary_mat(imagem, largura, altura, pygame.mouse.get_pos())
                 mask = [[0,0,0], [0,0,0], [0,0,0]]
                 result = floodfill_Morfologico(mat, pygame.mouse.get_pos(), largura, altura, mask)
-                result = mat_to_binary_img(result, largura, altura)
+                result = mat_to_binary_img(result, largura, altura, color_queue[color_pos])
+                fused_images = []
+                for i in range(largura):
+                    line = []
+                    for j in range(altura):
+                        if result[i][j].r == color_queue[color_pos][0] and result[i][j].g == color_queue[color_pos][1] and result[i][j].b == color_queue[color_pos][2]:
+                            line.append(result[i][j])
+                        else:
+                            line.append(imagem[i][j])
+                    fused_images.append(line)
                 for i in range(largura):
                     for j in range(altura):
-                        tela.set_at((i,j),(result[i][j].r,result[i][j].g, result[i][j].b))
+                        tela.set_at((i,j),(fused_images[i][j].r,fused_images[i][j].g, fused_images[i][j].b))
 
         if pressed == False:
             stop = False
